2fc38eb6a2f3116508dcdc9162b07c2cba507fe2
santy62kumar <s.santoshkumar6205@gmail.com>
2025-08-22 18:21:46 +0530

    deleetd files

--- src/controllers/index.js
+++
@@ -1,8 +0,0 @@
-// server/src/controllers/index.js
-const FeedbackController = require('./feedbackController');
-const MaintenanceController = require('./maintenanceController');
-
-module.exports = {
-  FeedbackController,
-  MaintenanceController
-};
--- src/services/external/index.js
+++
@@ -1,6 +0,0 @@
-// server/src/services/external/index.js
-// const twilioServices = require('./twilio');
-
-// module.exports = {
-//   twilio: twilioServices
-// };
